69c8f4291204d383e7f526694acf9717119529fd
Christopher Fenton <cjf_uk@hotmail.com>
2020-06-18 15:07:39 +0100

    Skeleton for the multi-demographic ensemble added

---
+++ metawards/demographic_ensemble/readme.md
@@ -0,0 +1,1 @@
+This is the multi-demographic ensemble (mixing and moving between populations) prepared for release from UQ4Covid. This is under construction
---
+++ metawards/demographic_ensemble/runscript.bat
@@ -0,0 +1,17 @@
+@echo off
+
+rem This assumes uq4metawards is installed
+rem This assumes that the data repo and the uq4covid repo are in the same folder
+
+if not exist "data" md "data"
+
+rem Call the pre-processor to fix the inputs for metawards
+
+uq4metawards-pre inputs\design.csv ..\..\..\uq4covid\model_config\metawards_multidemographic\limits.csv inputs\disease.csv -e -f
+
+rem Call metawards
+rem Note: to prevent spamming the repo, the output folder has been changed to a local one here, you'll need to change this
+
+if not exist ..\..\..\test md ..\..\..\test
+
+metawards -d ncov -D ..\..\..\uq4covid\model_config\metawards_multidemographic\demographics.json -o ..\..\..\test --force-overwrite-output --input inputs\disease.csv -a ExtraSeedsLondon.dat --extractor ..\..\..\uq4covid\model_config\metawards_multidemographic\mix_move_extractor::extract -u ..\..\..\uq4covid\model_config\metawards_multidemographicconstants.txt --iterator ..\..\..\uq4covid\model_config\metawards_multidemographic\iterate --mover ..\..\..\uq4covid\model_config\metawards_multidemographic\move_pathways --start-date 2020/01/01 --theme simple
